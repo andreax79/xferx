051a316b59d53f852922aa697c21c67eac8ebf4b
Andrea Bonomi <andrea.bonomi@gmail.com>
2026-08-13 21:47:47 +0200

    OS/32 tape backup test

---
+++ tests/dsk/os32mt.com
@@ -0,0 +1,14 @@
+CREATE/ALLOCATE:0 os32mt.tap
+INITIALIZE/os32mt os32mt.tap
+MOUNT/os32mt mag1: os32mt.tap
+COPY /type:co data/1.txt mag1:1.txt/0
+COPY /type:co data/2.txt mag1:2.txt/0
+COPY /type:co data/5.txt mag1:5.txt/0
+COPY /ascii data/10.txt mag1:10.txt/10
+COPY /ascii data/20.txt mag1:20.txt/10
+COPY /ascii data/50.txt mag1:50.txt/10
+COPY /ascii data/100.txt mag1:100.txt/20
+COPY /ascii data/200.txt mag1:200.txt/20
+COPY /ascii data/500.txt mag1:500.txt/20
+COPY /ascii data/1000.txt mag1:1000.txt/30
+COPY /ascii data/2000.txt mag1:2000.txt/30
---
+++ tests/test_os32mt.py
@@ -0,0 +1,86 @@
+import pytest
+
+from xferx.interdata.os32tapefs import OS32TapeFilesystem
+from xferx.shell import Shell
+
+DSK = "tests/dsk/os32mt.tap"
+
+
+def test_os32mt_read():
+    shell = Shell(verbose=True)
+    shell.onecmd(f"mount t: /os32mt {DSK}", batch=True)
+    fs = shell.volumes.get_volume('T')
+    assert isinstance(fs, OS32TapeFilesystem)
+
+    shell.onecmd("dir t:", batch=True)
+    shell.onecmd("dir /uic t:", batch=True)
+    shell.onecmd("type t:1.txt", batch=True)
+
+    x = fs.read_text("1000.txt")
+    assert len(x) == 44000
+    for i in range(0, 1000):
+        assert f"{i:5d} ABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890" in x
+
+    l = list(fs.entries_list)
+    assert len(l) == 11
+
+
+def test_os32mt_write():
+    shell = Shell(verbose=True)
+    shell.onecmd(f"copy {DSK} {DSK}.mo", batch=True)
+    shell.onecmd(f"mount in: /os32mt {DSK}", batch=True)
+    shell.onecmd(f"mount ou: /os32mt {DSK}.mo", batch=True)
+    fs = shell.volumes.get_volume('OU')
+    assert isinstance(fs, OS32TapeFilesystem)
+
+    d = fs.get_file_entry("500.TXT")
+    assert d is not None
+
+    # Delete a file
+    d.delete()
+    with pytest.raises(FileNotFoundError):
+        fs.get_file_entry("500.TXT")
+
+    # Create a file
+    shell.onecmd("copy /ascii in:10.TXT ou:10NEW.TXT", batch=True)
+    x1 = fs.read_text("10.txt")
+    assert len(x1) == 440
+    x2 = fs.read_text("10NEW.txt")
+    assert len(x2) == 440
+    for i in range(0, 10):
+        assert f"{i:5d} ABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890" in x2
+
+
+def test_os32mt_init():
+    shell = Shell(verbose=True)
+    shell.onecmd(f"mount in: /os32mt {DSK}", batch=True)
+    shell.onecmd(f"create /allocate:280 {DSK}.mo", batch=True)
+    shell.onecmd(f"init /os32mt {DSK}.mo", batch=True)
+    shell.onecmd(f"mount ou: /os32mt {DSK}.mo", batch=True)
+    shell.onecmd("dir ou:", batch=True)
+    shell.onecmd("copy in:*.TXT ou:", batch=True)
+    shell.onecmd("copy in:*.TXT ou:", batch=True)
+    shell.onecmd("dir ou:", batch=True)
+
+    fs = shell.volumes.get_volume('OU')
+    l = list(fs.entries_list)
+    assert len(l) == 11
+
+    x = fs.read_text("1000.txt")
+    assert len(x) == 44000
+    for i in range(0, 1000):
+        assert f"{i:5d} ABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890" in x
+
+    l0 = len(list(fs.filter_entries_list("*.txt", account=0, wildcard=True)))
+    assert l0 == 3
+    l10 = len(list(fs.filter_entries_list("*.txt", account=10, wildcard=True)))
+    assert l10 == 3
+
+    shell.onecmd("del ou:*.txt/10", batch=True)
+    l10 = len(list(fs.filter_entries_list("*.txt", account=10, wildcard=True)))
+    assert l10 == 0
+
+    # Test init mounted volume
+    shell.onecmd("init ou:", batch=True)
+    with pytest.raises(Exception):
+        fs.read_bytes("1000.txt")
